7e19d2720c857981f5bb598794d0887f9ff8d314
Julia Naomi <jnaomi@thoughtworks.com>
2017-09-19 22:24:26 -0300

    bumping the server

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
 extern crate hyper;
-extern crate service_fn;
+extern crate futures;
 
-use hyper::header::{ContentLength, ContentType};
-use hyper::server::{Http, Response};
-use service_fn::service_fn;
+use futures::future::Future;
 
-static TEXT: &'static str = "Hello, World!";
-
-fn run() -> Result<(), hyper::Error> {
-    let addr = ([127, 0, 0, 1], 3000).into();
-
-    let hello = || Ok(service_fn(|_req|{
-        Ok(Response::<hyper::Body>::new()
-            .with_header(ContentLength(TEXT.len() as u64))
-            .with_header(ContentType::plaintext())
-            .with_body(TEXT))
-    }));
-
-    let server = Http::new().bind(&addr, hello)?;
-    server.run()
-}
+use hyper::header::ContentLength;
+use hyper::server::{Http, Request, Response, Service};
 
 fn main() {
-    run();
+    let addr = "127.0.0.1:3000".parse().unwrap();
+    let servidor = Http::new().bind(&addr, || Ok(Ola)).unwrap();
+    servidor.run().unwrap();
+}
+
+struct Ola;
+
+const FRASE: &'static str = "Ola Goku!";
+
+impl Service for Ola {
+
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
+
+    fn call(&self, _req: Request) -> Self::Future {
+        Box::new(futures::future::ok(
+            Response::new()
+                .with_header(ContentLength(FRASE.len() as u64))
+                .with_body(FRASE)
+        ))
+    }
 }
