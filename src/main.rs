0ee4d419a3c9c243d51fff048071eb5285d3fd18
Julia Naomi <jnaomi@thoughtworks.com>
2017-09-19 23:07:24 -0300

    routing a post

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,41 @@ extern crate futures;
 
 use futures::future::Future;
 
-use hyper::header::ContentLength;
+use hyper::{Method, StatusCode};
 use hyper::server::{Http, Request, Response, Service};
 
 fn main() {
     let addr = "127.0.0.1:3000".parse().unwrap();
-    let servidor = Http::new().bind(&addr, || Ok(Ola)).unwrap();
+    let servidor = Http::new().bind(&addr, || Ok(DbzServer)).unwrap();
     servidor.run().unwrap();
 }
 
-struct Ola;
+struct DbzServer;
 
 const FRASE: &'static str = "Ola Goku!";
 
-impl Service for Ola {
+impl Service for DbzServer {
 
     type Request = Request;
     type Response = Response;
     type Error = hyper::Error;
     type Future = Box<Future<Item=Self::Response, Error=Self::Error>>;
 
-    fn call(&self, _req: Request) -> Self::Future {
-        Box::new(futures::future::ok(
-            Response::new()
-                .with_header(ContentLength(FRASE.len() as u64))
-                .with_body(FRASE)
-        ))
+    fn call(&self, req: Request) -> Self::Future {
+        let mut response = Response::new();
+
+        match (req.method(), req.path()) {
+            (&Method::Get, "/") => {
+                response.set_body(FRASE);
+            },
+            (&Method::Post, "/dbz") => {
+                response.set_body(req.body());
+            },
+            _ => {
+                response.set_status(StatusCode::NotFound);
+            },
+        };
+
+        Box::new(futures::future::ok(response))
     }
 }
